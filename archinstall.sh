169637d2e8b9a94d0fca3130f2f3987986180e00
nicky <nickyknowles186@gmail.com>
2026-01-20 19:44:54 +0000

    added powerlevel10k config, symlinked. fixed .zshrc config

--- archinstall.sh
+++ archinstall.sh
@@ -1,23 +1,23 @@
 
 # Pacman packages
-sudo pacman -Syu base-devel cava cmake fastfetch flatpak foot gcc git gnome-font-viewer hyprland lolcat nano obs-studio pavucontrol sddm spotify-player tmux waybar zsh discord swww nautilus wofi xdg-desktop-portal-hyprland
+sudo pacman -Syu base-devel cava cmake fastfetch flatpak foot gcc git gnome-font-viewer hyprland lolcat nano obs-studio pavucontrol sddm spotify-player tmux waybar zsh discord swww nautilus wofi xdg-desktop-portal-hyprland thefuck
 
 # System services
 sudo systemctl enable sddm
 
-#Installing AUR
+# Installing AUR
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 cd ~
 
-#Installing vencord
+# Installing vencord
 sh -c "$(curl -sS https://vencord.dev/install.sh)"
 
-#Installing yay packages
+# Installing yay packages
 yay -Syu pywal-discord tty-clock pywalfox pywal oomox themix
 
-#Symlinking configs
+# Symlinking configs
 ln -s ~/customrice/hypr/ ~/.config/
 ln -s ~/customrice/waybar ~/.config/
 
@@ -30,10 +30,10 @@ ln -s ~/customrice/wallpaper ~/.local/bin
 sudo rm /etc/xdg/foot/foot.ini
 sudo ln -s ~/customrice/foot.ini /etc/xdg/foot/
 
-#Setting up omz
+# Setting up omz
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+rm ~/.zshrc
+ln -s ~/customrice/.zshrc ~
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
-
-rm ~/.zshrc
-# ln -s ~/customrice/.zshrc /
+ln -s ~/customrice/powerlevel10k ~/.oh-my-zsh/custom/themes/
